9a16a8e1f5896e7f95014d1a3e861dd1e4064dc4
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2022-08-15 13:53:07 +0200

    install dependencies | set up basic socket connection

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const httpServer = createServer(app);
+
+app.use(cors());
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  },
+});
+
+io.on('connection', socket => {
+  console.log(socket);
+});
+
+httpServer.listen(3000);
