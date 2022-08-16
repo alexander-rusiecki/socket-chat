1ee90cd889c054557a2438ad08a328e3e557e065
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2022-08-16 08:17:25 +0200

    install socket for client | add messaging functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
+import { useState } from 'react';
+import oi from 'socket.io-client';
+const socket = oi('http://localhost:4000');
+
 function App() {
+  const [chatMessage, setChatMessage] = useState('');
+  function sendMsg(e) {
+    e.preventDefault();
+    socket.emit('chat message', { chatMessage });
+  }
   return (
     <div>
-      <h1>YOoo</h1>
+      <h1>Chat app</h1>
+      <form onSubmit={sendMsg}>
+        <input
+          type="text"
+          name="chat"
+          id="chat"
+          placeholder="message"
+          onChange={e => setChatMessage(e.target.value)}
+        />
+        <button>Send</button>
+      </form>
     </div>
   );
 }
