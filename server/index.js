6980934a3cabaeba295db6d532325fe2bee5fff2
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2022-08-18 23:01:22 +0200

    deploy

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 const io = new Server(httpServer, {
   cors: {
-    origin: ['http://localhost:3000', 'https://socket-chat-rho.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://socket-chat-rho.vercel.app'],
     methods: ['GET', 'POST'],
     credentials: true,
   },
